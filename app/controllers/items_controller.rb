b66c4ca62281c9a4750038d426e1ac3999bbfc95
Connie Park <conniedc1206@gmail.com>
2022-07-20 10:42:13 -0700

    commit tree 9eb86327793cb97aca3739545ea76e8046bcd828

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  # GET /users/:user_id/items
+  # OR GET /items
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  # GET /users/:user_id/items/:id
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  # POST /users/:user_id/items
+  def create
+    user = User.find(params[:user_id])
+    new_item = Item.create(item_params)
+    render json: new_item, status: :created
+  end
+
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Review not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
 end
